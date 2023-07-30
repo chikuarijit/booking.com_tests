f81b3c6f250c5d724579deaa51bf6dfa50bf8f08
chikuarijit <chikuarijitdas@gmail.com>
2023-07-30 17:10:11 +0530

    Fourth Commit

--- tests/test_opening_landing_page.py
+++ tests/test_opening_landing_page.py
@@ -1,20 +1,25 @@
 import slash
 from slash_step import STEP
 from .booking_base_test import BookingBaseTest
-from my_ui_booking import booking, change_currency, change_language
+from my_ui_booking import booking, \
+    change_currency, change_language
 
 
 class LandingPage(BookingBaseTest):
     def before(self):
         super().before()
-        self.booking_instance = booking.Booking(self.driver)
-        self.currency_changer = change_currency.ChangeCurrency(self.driver)
-        self.language_changer = change_language.ChangeLanguage(self.driver)
+        self.booking_instance = booking.\
+            Booking(self.driver)
+        self.currency_changer = change_currency.\
+            ChangeCurrency(self.driver)
+        self.language_changer = change_language.\
+            ChangeLanguage(self.driver)
 
     @slash.tag("First")
     def test_opening_landing_page(self):
 
-        with STEP("Step 1 : Preconditions: Open Booking.com home page"):
+        with STEP("Step 1 : Preconditions: "
+                  "Open Booking.com home page"):
             self.booking_instance.open_landing_page()
             self.booking_instance.handle_popup()
 
@@ -26,8 +31,7 @@ class LandingPage(BookingBaseTest):
 
         with STEP("Step 3: Open Language Page"):
             self.language_changer.launch()
-            assert self.currency_changer.loaded, \
+            assert self.language_changer.loaded, \
                 "Language page not loaded"
-
-            
-
+            self.language_changer.\
+                select_language("English (UK)")
